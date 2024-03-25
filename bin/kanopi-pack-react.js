5e931cdac7c7fbd59bd39f443bfe3a54e8e86218
Ryan Leeson <ryan.leeson@kanopistudios.com>
2024-03-25 12:25:10 -0400

    Watch Mode (#23)
    * Add Watch capability to match the feature added to Kanopi Pack Standard
    * Add Watch configuration check

--- bin/kanopi-pack-react.js
+++ bin/kanopi-pack-react.js
@@ -6,46 +6,50 @@ const utility = require('util');
 const {
   chalk,
   standardPackage,
-  react: { development: developmentConfig, production: productionConfig }
+  react: { development, production }
 } = require(path.resolve(__dirname, '..', 'index'));
 const {
   commands: { standard: program },
-  runners: { runDevServer, runWebpack }
+  environment: { standard: { watchOptions } },
+  runners: { runDevServer, runWebpack, watchWebpack }
 } = standardPackage;
+
+const PACKAGE_MODES = {
+  'development': { configuration: development, name: 'Development', runner: () => runDevServer(development) },
+  'production': { configuration: production, name: 'Production', runner: () => runWebpack(production) },
+  'watch': { configuration: production, name: 'Watch', runner: () => watchWebpack(production, watchOptions) }
+};
 
 program
   .command('check-configuration')
   .description('Output the Webpack configuration for the specified environment.')
   .argument('[depth]', 'Number of levels deep (default 6) to show the configuration')
-  .argument('[environment]', 'Choose production (default) or development')
+  .argument('[environment]', 'Choose production (default), development, or watch')
   .argument('[color]', 'Whether to show the output in color (default false)')
   .action((depth = 6, environment = 'production', color = false) => {
-    const isDevelopment = 'development' === environment;
+    const selectedMode = PACKAGE_MODES[environment] ?? PACKAGE_MODES['production'];
+    const { configuration, name } = selectedMode;
 
     console.log(chalk.greenBright('Package:\tKanopi Pack React'))
-    console.log(chalk.yellow('Environment:\t' + (isDevelopment ? 'Development' : 'Production')));
+    console.log(chalk.yellow('Environment:\t' + name));
     console.log('');
     console.log(chalk.yellow('Current configuration:'));
     console.log('');
-    console.log(utility.inspect(
-      isDevelopment ? developmentConfig : productionConfig,
-      { depth: depth, colors: color }
-    ));
+    console.log(utility.inspect(configuration, { depth: depth, colors: color }));
   });
 
 program
   .command('react')
-  .description('Run React application builds, set environment to development for HMR')
-  .argument('[environment]', 'Choose production (default) or development')
+  .description('Run React application builds, set environment to development for HMR, or watch for rebuilds without HMR.')
+  .argument('[environment]', 'Choose production (default), development, or watch')
   .action((environment) => {
-    const isDevelopment = 'development' === environment;
+    const selectedMode = PACKAGE_MODES[environment] ?? PACKAGE_MODES['production'];
+    const { name, runner } = selectedMode;
 
     console.log(chalk.greenBright('Package:\tKanopi Pack React'))
-    console.log(chalk.yellow('Environment:\t' + (isDevelopment ? 'Development' : 'Production')));
+    console.log(chalk.yellow('Environment:\t' + name));
     console.log('');
-    isDevelopment
-      ? runDevServer(developmentConfig)
-      : runWebpack(productionConfig);
+    runner();
   });
 
 program.parse(process.argv);
--- index.js
+++ index.js
@@ -2,9 +2,9 @@ const path = require('path');
 const package = require(path.resolve(__dirname, 'standard-loader'))();
 
 module.exports = {
-    ...package,
-    react: {
-        development: require(path.resolve(__dirname, 'configuration', 'development')),
-        production: require(path.resolve(__dirname, 'configuration', 'production'))
-    }
+  ...package,
+  react: {
+    development: require(path.resolve(__dirname, 'configuration', 'development')),
+    production: require(path.resolve(__dirname, 'configuration', 'production'))
+  }
 };
